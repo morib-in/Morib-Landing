008918bf0915cb39b198f282fac82c0e32b0c361
hanseo <yds06206@gmail.com>
2025-02-26 18:04:54 +0900

    feat: DetailSection 컴포넌트 생성

---
+++ src/components/DetailSection.tsx
@@ -0,0 +1,13 @@
+import TextBlock from '@/components/TextBlock';
+import MediaBlock from '@/components/MediaBlock';
+
+const DetailSection = () => {
+  return (
+    <div className="flex gap-[2rem] pl-[24rem]">
+      <TextBlock />
+      <MediaBlock />
+    </div>
+  );
+};
+
+export default DetailSection;
