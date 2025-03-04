73de6f3f8f838b8186156d36a62e10165097bb04
KIMGEONHWI <dirnrlarjsgn@naver.com>
2025-03-04 16:03:01 +0900

    feat: 다운로드 버튼 구현 (#12)

---
+++ src/components/DownloadBuutton.tsx
@@ -0,0 +1,9 @@
+const DownloadBuutton = () => {
+  return (
+    <button className="rounded-[10px] border border-mint-02 bg-gray-bg-02 px-[3rem] py-[1.5rem] text-mint-02 head-bold-24">
+      모립 다운로드 하기
+    </button>
+  );
+};
+
+export default DownloadBuutton;
