facd21c1228c46c204a130509fa651741f1a26d9
KIMGEONHWI <dirnrlarjsgn@naver.com>
2025-03-04 17:11:09 +0900

    style: 다운로드 버튼 hover 적용 (#12)

--- src/components/DownloadBuutton.tsx
+++ src/components/DownloadBuutton.tsx
@@ -1,6 +1,6 @@
 const DownloadBuutton = () => {
   return (
-    <button className="rounded-[10px] border border-mint-02 bg-gray-bg-02 px-[1.8rem] py-[1rem] text-mint-02 detail-semibold-14 sm:px-[3rem] sm:py-[1.5rem] sm:head-bold-24">
+    <button className="rounded-[10px] border border-mint-02 bg-gray-bg-02 px-[1.8rem] py-[1rem] text-mint-02 transition-colors detail-semibold-14 hover:border-mint-02-hover hover:bg-gray-bg-03 hover:text-mint-02-hover sm:px-[3rem] sm:py-[1.5rem] sm:head-bold-24">
       모립 다운로드 하기
     </button>
   );
--- src/components/FinalSection.tsx
+++ src/components/FinalSection.tsx
@@ -3,9 +3,9 @@ import DownloadBuutton from './DownloadBuutton';
 
 const FinalSection = () => {
   return (
-    <section className="flex w-full flex-col items-center justify-center gap-[4.8rem] py-[4rem] sm:gap-[6rem]">
+    <section className="flex w-full flex-col items-center justify-center gap-[4.8rem] sm:gap-[6rem] sm:py-[4rem]">
       <div className="flex flex-col items-center justify-center gap-[2rem]">
-        <h2 className="sm:title-bold-72 title-bold-32">지금 바로 몰입하세요</h2>
+        <h2 className="title-bold-32 sm:title-bold-72">지금 바로 몰입하세요</h2>
         <DownloadBuutton />
       </div>
       <Image src="/section6.png" alt="섹션6 이미지" width={1440} height={1080} />
