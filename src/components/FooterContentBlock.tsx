b6baf313c4ea31179ffcd81b48e5a7eda2ad03a8
ivoryeee <psa109@sookmyung.ac.kr>
2025-03-04 11:53:40 +0900

    refactor: 내부에서 footer contents items 직접 렌더링하도록 로직 수정 (#10)

--- src/components/FooterContentBlock.tsx
+++ src/components/FooterContentBlock.tsx
@@ -6,19 +6,6 @@ interface FooterContentProps {
 }
 
 const FooterContentBlock = ({title, items}: FooterContentProps) => {
-  const renderFooterItems = (items: FooterItem[]) => {
-    return items.map((item, index) => (
-      typeof item === 'string' ? (
-        <li key={index} className="self-stretch text-gray-04 subhead-med-18 max-w-[27.4rem] break-words">
-          {item}
-        </li>
-      ) : (
-        <li key={index} className="flex items-center gap-[2rem] self-stretch">
-          <span className="self-stretch text-gray-04 subhead-med-18">{item.label}</span>
-          <span className="self-stretch text-gray-04 subhead-med-18">{item.value}</span>          </li>
-      )
-    ));
-  }; 
 
   return (
     <div className="flex flex-col w-full gap-[2rem]">
@@ -26,7 +13,22 @@ const FooterContentBlock = ({title, items}: FooterContentProps) => {
       {title}
     </h3>
     <ul className="flex flex-col items-start gap-[1rem] self-stretch">
-      {renderFooterItems(items)}
+      {items.map((item, index) =>
+        typeof item === "string" ? (
+        <li key={index} className="self-stretch text-gray-04 subhead-med-18 max-w-[27.4rem] break-words">
+          {item}
+        </li>
+        ) : (
+        <li key={index} className="flex items-center gap-[2rem] self-stretch">
+          <span className="self-stretch text-gray-04 subhead-med-18">
+            {item.label}
+          </span>
+          <span className="self-stretch text-gray-04 subhead-med-18">
+            {item.value}
+          </span>
+        </li>
+        )
+      )}
     </ul>
   </div>
   );
