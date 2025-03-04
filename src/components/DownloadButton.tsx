6beb700cdf3944ea76b858a52cbdef319472c81c
KIMGEONHWI <dirnrlarjsgn@naver.com>
2025-03-04 17:28:05 +0900

    codereview: 한서 코드리뷰 반영 (#12)

---
+++ src/components/DownloadButton.tsx
@@ -0,0 +1,11 @@
+const DownloadButton = () => {
+  return (
+    <button
+      type="button"
+      className="rounded-[10px] border border-mint-02 bg-gray-bg-02 px-[1.8rem] py-[1rem] text-mint-02 transition-colors detail-semibold-14 hover:border-mint-02-hover hover:bg-gray-bg-03 hover:text-mint-02-hover sm:px-[3rem] sm:py-[1.5rem] sm:head-bold-24">
+      모립 다운로드 하기
+    </button>
+  );
+};
+
+export default DownloadButton;
--- src/components/FinalSection.tsx
+++ src/components/FinalSection.tsx
@@ -1,12 +1,12 @@
 import Image from 'next/image';
-import DownloadBuutton from './DownloadBuutton';
+import DownloadButton from './DownloadButton';
 
 const FinalSection = () => {
   return (
     <section className="flex w-full flex-col items-center justify-center gap-[4.8rem] sm:gap-[6rem] sm:py-[4rem]">
       <div className="flex flex-col items-center justify-center gap-[2rem]">
         <h2 className="title-bold-32 sm:title-bold-72">지금 바로 몰입하세요</h2>
-        <DownloadBuutton />
+        <DownloadButton />
       </div>
       <Image src="/section6.png" alt="섹션6 이미지" width={1440} height={1080} />
     </section>
