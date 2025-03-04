d42003831863181499f2009b6f80a485152eb83c
Hanseo Kim <108131226+seueooo@users.noreply.github.com>
2025-03-04 13:21:32 +0900

    Feat: 무한 이동 슬로건 구현 (#9)
    * style: 누락된 스타일 추가
    
    * feat: infiniteSlogan 컴포넌트 생성
    
    * style: 애니메이션 추가
    
    * feat: style jsx으로 keyframe 정의, 미디어 쿼리로 화면 크기에 따라 슬라이드 이동 넓이가 달라지도록 구현
    
    * remove: 필요없는 코드 삭제
    
    * refactor: 반복적인 요소 map함수 사용하도록 수정
    
    * refactor: 슬로건 svg 파일명 변경, alt 속성 추가

---
+++ src/components/InfiniteSlogan.tsx
@@ -0,0 +1,68 @@
+import Image from 'next/image';
+
+interface Props {
+  className?: string;
+}
+
+const InfiniteSlogan = ({ className }: Props) => {
+  const images = Array(4).fill({
+    src: '/slogan.svg',
+    alt: 'A WORKSPACE FOR IMMERSION - Morib slogan',
+    width: 926,
+    height: 49,
+  });
+  return (
+    <div className={`relative w-full overflow-hidden ${className}`}>
+      <style jsx>{`
+        .slider {
+          display: flex;
+          will-change: transform;
+        }
+
+        @media (max-width: 639px) {
+          .slider {
+            animation: slideMobile 5s linear infinite;
+          }
+
+          @keyframes slideMobile {
+            0% {
+              transform: translateX(0);
+            }
+            100% {
+              transform: translateX(calc(-50% - 0.5rem));
+            }
+          }
+        }
+
+        @media (min-width: 640px) {
+          .slider {
+            animation: slideDesktop 3s linear infinite;
+          }
+
+          @keyframes slideDesktop {
+            0% {
+              transform: translateX(0);
+            }
+            100% {
+              transform: translateX(calc(-50% - 2.7rem));
+            }
+          }
+        }
+      `}</style>
+      <div className="slider flex gap-[0.5rem] sm:gap-[2.7rem]">
+        {images.map((image, index) => (
+          <Image
+            key={index}
+            src={image.src}
+            alt={image.alt}
+            width={image.width}
+            height={image.height}
+            className="block w-1/2"
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default InfiniteSlogan;
