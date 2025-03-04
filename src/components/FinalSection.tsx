641661e0e84f9d262417fb09660f3eb4df8f39d9
KIMGEONHWI <dirnrlarjsgn@naver.com>
2025-03-04 21:33:19 +0900

    codereview: 상아 코리 반영 (#12)

--- src/components/FinalSection.tsx
+++ src/components/FinalSection.tsx
@@ -1,11 +1,12 @@
 import Image from 'next/image';
 import DownloadButton from './DownloadButton';
+import { FINAL_SECTION_CONTENT } from '@/constants/contents';
 
 const FinalSection = () => {
   return (
     <section className="flex w-full flex-col items-center justify-center gap-[4.8rem] sm:gap-[6rem] sm:py-[4rem]">
       <div className="flex flex-col items-center justify-center gap-[2rem]">
-        <h2 className="title-bold-32 sm:title-bold-72">지금 바로 몰입하세요</h2>
+        <h2 className="title-bold-32 sm:title-bold-72">{FINAL_SECTION_CONTENT.title}</h2>
         <DownloadButton />
       </div>
       <Image src="/section6.svg" alt="모립 타이머 이미지" width={1440} height={1080} />
